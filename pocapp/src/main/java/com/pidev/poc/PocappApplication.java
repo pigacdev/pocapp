640586799303d83875e8405cfbf679abbead1e06
ipigac <ivanpigac@serengetitech.com>
2021-09-29 09:12:55 +0200

    Inicijalna verzija

---
+++ pocapp/src/main/java/com/pidev/poc/PocappApplication.java
@@ -0,0 +1,13 @@
+package com.pidev.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocappApplication.class, args);
+	}
+
+}
